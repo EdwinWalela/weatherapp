77170ce3c2969914b8ba08b1b2d4f4baf56b22d3
edwinwalela <edwinwalela@gmail.com>
2023-01-09 13:17:36 +0300

    get dynamic date

--- src/pages/Landing/header.tsx
+++ src/pages/Landing/header.tsx
@@ -2,12 +2,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+	const now = new Date();
+	let months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+	let monthsShort = [
+		'Jan',
+		'Feb',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'Aug',
+		'Sept',
+		'Oct',
+		'Nov',
+		'Dec',
+	];
+
+	let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+	let month = months[now.getMonth()];
+	let monthShort = monthsShort[now.getMonth()];
+	let day = days[now.getDay()];
+
 	return (
 		<div className="flex-1 ">
 			<div className="flow-root ">
 				<div className="float-left">
-					<h2 className="font-bold text-2xl">January</h2>
-					<p className="text-gray-500 text-sm">Thursday, Jan 4 2022</p>
+					<h2 className="font-bold text-2xl">{month}</h2>
+					<p className="text-gray-500 text-sm">
+						{day}, {monthShort} {now.getDate()} {now.getFullYear()}
+					</p>
 				</div>
 				<div className="float-right">
 					<input
